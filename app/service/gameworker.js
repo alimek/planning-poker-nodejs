2d9632f937cbbc6a67aecca8ac3d011e49ec7ec9
Grzegorz Mandziak <alimek@alimek.pl>
2016-06-14 22:55:17 +0200

    Joining game, leaving game and sending current players when join

--- app/service/gameworker.js
+++ app/service/gameworker.js
@@ -26,21 +26,40 @@ module.exports = (io, rabbitService) => {
     });
 
     io.on('connection', (socket) => {
+        //TODO: Refactor it
         console.log('New connection', socket.id, "\n");
-        socket.on('game', (gameID) => {
-          console.log(socket.id, 'joined to ', gameID);
+        socket.on('game', (connectionToGame) => {
+          var gameID = connectionToGame.gameID;
+          var user = connectionToGame.user;
           var game = _.find(games, {id: gameID});
+          var players = [];
+
+          console.log(socket.id, '-', user.name , 'joined to ', gameID);
+
           if (_.isUndefined(game)) {
             game = new GameModel(gameID, 'asdf');
             games.push(game);
+          } else {
+            _.forEach(game.sockets, (player) => {
+              players.push(player.user);
+            });
           }
+
           socket.game = game;
+          socket.user = user;
           game.sockets.push(socket);
           socket.join('game-'+gameID);
+
+
+          socket.emit('players', players);
+          socket.broadcast.to('game-'+gameID).emit('new-player', user);
         });
 
         socket.on('disconnect', () => {
+          if (!_.isUndefined(socket.game)) {
+            socket.broadcast.to('game-'+socket.game.id).emit('player-leaved', socket.user.id);
             socket.game.removeSocket(socket);
+          }
         });
     });
 };
